6d6bb3fdcebb085cd2c777701153e41dd55b022f
Bethuel <bethropolis@gmail.com>
2024-01-11 00:54:16 +0300

    created header.php
    moved header info into the header.php file (new)

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/Logo icon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="./asserts/css/style.css">
+    <link rel="stylesheet" href="./asserts/libs/font-awesome/css/all.min.css">
+    <title>Box champy</title>
+</head>
+<body>
